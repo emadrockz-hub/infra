faa3f82f79c5e101132a57592a0d334ad2c1eb47
emadrockz-hub <emadrockz@gmail.com>
2026-01-21 19:47:28 -0500

    Update Terraform configuration for required version

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-required_providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = ">= 5.110.0, < 6.0.0"
+terraform {
+  required_version = "= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.110.0, < 6.0.0"
+    }
   }
-}
 }
 
 provider "aws" {
